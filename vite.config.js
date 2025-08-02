09823b46a366b97668559d282d2254015240c7aa
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2025-08-02 10:37:10 +0600

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: "92.205.234.255",
-    host: "10.10.7.47",
+    host: "92.205.234.255",
+    // host: "10.10.7.47",
     port: 5000,
   },
 });
